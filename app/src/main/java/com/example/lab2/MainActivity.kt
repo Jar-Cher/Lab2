ef18be702b42d1434cabdb80203b8023c5118563
Jaroslav <jaroslav.chernyshev@gmail.com>
2020-10-02 23:30:21 +0300

    Initial commit

---
+++ app/src/main/java/com/example/lab2/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.lab1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.example.lab2.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d("CREATION", "Created!")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("START", "Started!")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("RESTART", "Restarted!")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("PAUSE", "Paused!")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("RESUME", "Resume!")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("DESTROY", "Destroyed!")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("STOP", "Stopped!")
+    }
+}
